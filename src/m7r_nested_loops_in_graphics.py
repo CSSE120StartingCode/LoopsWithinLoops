0df96bf333075d14bd3e66f4fd07fd3750eeab54
David Fisher <fisherds@rose-hulman.edu>
2021-06-07 09:27:16 -0400

    updates for 2021

--- src/m7r_nested_loops_in_graphics.py
+++ src/m7r_nested_loops_in_graphics.py
@@ -2,11 +2,31 @@
 This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
-Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+Authors: David Mutchler, Vibha Alangar, Dave Fisher, Matt Boutell, Mark Hays,
+         Mohammed Noureddine, Sana Ebrahimi, Sriram Mohan, their colleagues and
+         PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
+
+
+# -----------------------------------------------------------------------------
+# TODO: 2.
+#  *** Unless your instructor directs you otherwise,
+#      IF YOU HAVE NOT ALREADY WATCHED THE VIDEO
+#          nested_loops_in_GRAPHICS.mp4
+#      in the PREPARATION for this session
+#          do so ** NOW **
+#      As the video proceeds, LOOK AT THE CODE BELOW.
+#      It is the same as in the video.
+#  __
+#  *** USE THE VIDEO to understand the TECHNIQUES used in BOTH examples.
+#  __
+#   AFTER you have watched the video, asking questions as needed,
+#   and you feel that you understand the TECHNIQUES it presents, THEN:
+#  __
+#     *** Change the _TODO_ above to DONE. ***
+# -----------------------------------------------------------------------------
 
 
 def main():
@@ -20,58 +40,39 @@ def nested_loops_in_graphics_example():
     """
     width = 800
     height = 600
-    title = 'Rectangles and Triangles of Circles'
+    title = "Rectangles and Triangles of Circles"
     window = rg.RoseWindow(width, height, title)
 
-    window.continue_on_mouse_click('Click to run Example 1.')
+    window.continue_on_mouse_click("Click to run Example 1.")
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     starting_point = rg.Point(50, 50)
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # First set of circles
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     radius = 20
     starting_circle = rg.Circle(starting_point, radius)
 
     rectangle_of_circles(window, starting_circle.clone(), 4, 12)
-    window.continue_on_mouse_click('Click to run Example 2.')
+    window.continue_on_mouse_click("Click to run Example 2.")
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Second set of circles
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     starting_circle.move_by(180, 400)
 
     rectangle_of_circles(window, starting_circle.clone(), 14, 2)
-    window.continue_on_mouse_click('Click to run Example 3.')
+    window.continue_on_mouse_click("Click to run Example 3.")
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Third and last set of circles
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     starting_circle.move_by(200, -400)
 
     triangle_of_circles(window, starting_circle.clone(), 8)
     window.close_on_mouse_click()
-
-# ----------------------------------------------------------------------
-# TODO: 2.
-#  *** Unless your instructor directs you otherwise,
-#      watch the video
-#          nested_loops_in_GRAPHICS.mp4
-#      in Preparation for Session 18
-#          ** NOW **
-#      As the video proceeds, LOOK AT THE CODE BELOW.
-#      It is the same as the video.
-#      (Pause the video when it completes the following problem.)
-#
-#  *** USE THE VIDEO to understand the TECHNIQUE used in this example.
-#
-#   AFTER you have watched the video, asking questions as needed,
-#   and you feel that you understand the TECHNIQUE it presents, THEN:
-#
-#     *** Change the TO DO above to DONE. ***
-# ----------------------------------------------------------------------
 
 
 def rectangle_of_circles(window, circle, m, n):
@@ -107,26 +108,6 @@ def rectangle_of_circles(window, circle, m, n):
         x = original_x  # Reset x to the left-edge, for the next row
 
 
-# ----------------------------------------------------------------------
-# TODO: 3.
-#  *** Unless your instructor directs you otherwise,
-#      watch the video
-#          nested_loops_in_GRAPHICS.mp4
-#      in Preparation for Session 18
-#          ** NOW **
-#      As the video proceeds, LOOK AT THE CODE BELOW.
-#      It is the same as the video.
-#      (Continue the video from where you paused it in the previous problem.)
-#
-#  *** USE THE VIDEO to understand the TECHNIQUE used in this example.
-#
-#   AFTER you have watched the video, asking questions as needed,
-#   and you feel that you understand the TECHNIQUE it presents, THEN:
-#
-#     *** Change the TO DO above to DONE. ***
-# ----------------------------------------------------------------------
-
-
 def triangle_of_circles(window, circle, n):
     """
     Draws an n x n right-triangle of circles
@@ -142,11 +123,11 @@ def triangle_of_circles(window, circle, n):
       :type n: int
     and m is a small, positive integer.
     """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # NOTE: The solution below is IDENTICAL to the rectangle_of_circles
     #       solution except that the INNER loop has  j+1  instead of m.
     # Make sure you understand why this works!
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     original_x = circle.center.x
     original_y = circle.center.y
     radius = circle.radius
@@ -165,7 +146,7 @@ def triangle_of_circles(window, circle, n):
         x = original_x  # Reset x to the left-edge, for the next row
 
 
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 main()
--- src/m8_nested_loops_in_graphics.py
+++ src/m8_nested_loops_in_graphics.py
@@ -2,8 +2,9 @@
 This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
-Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+Authors: David Mutchler, Vibha Alangar, Dave Fisher, Matt Boutell, Mark Hays,
+         Mohammed Noureddine, Sana Ebrahimi, Sriram Mohan, their colleagues and
+         PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
@@ -11,8 +12,13 @@ import rosegraphics as rg
 
 def main():
     """ Calls the other functions to demonstrate them. """
-    run_test_draw_L()
-    run_test_draw_wall_on_right()
+
+    print("-----------------------------------------------")
+    print("Un-comment each of the following TEST functions")
+    print("as you implement the functions that they test.")
+    print("-----------------------------------------------")
+    # run_test_draw_L()
+    # run_test_draw_wall_on_right()
 
 
 def run_test_draw_L():
@@ -21,34 +27,34 @@ def run_test_draw_L():
     """
     width = 800
     height = 600
-    title = 'Draw an L of circles.  Two tests'
+    title = "Draw an L of circles.  Two tests"
     window = rg.RoseWindow(width, height, title)
 
-    window.continue_on_mouse_click('Click to run Test 1.')
+    window.continue_on_mouse_click("Click to run Test 1.")
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     starting_point = rg.Point(50, 50)
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # First L.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     radius = 10
     starting_circle = rg.Circle(starting_point, radius)
     green_circle = starting_circle.clone()
-    green_circle.fill_color = 'green'
+    green_circle.fill_color = "green"
 
     draw_L(window, green_circle, 10, 5)
-    window.continue_on_mouse_click('Click to run Test 2.')
+    window.continue_on_mouse_click("Click to run Test 2.")
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Second L.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     starting_circle.move_by(250, 100)
     blue_circle = starting_circle.clone()
-    blue_circle.fill_color = 'blue'
+    blue_circle.fill_color = "blue"
 
-    window.continue_on_mouse_click('Click to run Test 2.')
+    window.continue_on_mouse_click("Click to run Test 2.")
     draw_L(window, blue_circle, 6, 15)
 
     window.close_on_mouse_click()
@@ -56,15 +62,15 @@ def run_test_draw_L():
 
 def draw_L(window, circle, r, c):
     """
-    See   L.pdf   in this project for pictures that may
+    See   pictures_for_L.pdf   in this project for pictures that may
     help you better understand the following specification:
 
-    Draws an 'L' of circles on the given rg.RoseWindow.
-      The 'column' part of the L should have r rows and 3 columns.
-        (That is, it is r 'tall' and 3 'thick'.)
-      The 'shared corner' part of the L should be 3 x 3.
-      The 'row' part of the L should have c columns and 3 rows.
-        (That is, it is c 'long' and 3 'thick'.)
+    Draws an "L" of circles on the given rg.RoseWindow.
+      The "column" part of the L should have r rows and 3 columns.
+        (That is, it is r "tall" and 3 "thick".)
+      The "shared corner" part of the L should be 3 x 3.
+      The "row" part of the L should have c columns and 3 rows.
+        (That is, it is c "long" and 3 "thick".)
 
       The given rg.Circle specifies:
       - The position of the upper-left circle drawn and also
@@ -79,23 +85,23 @@ def draw_L(window, circle, r, c):
       :type c: int
     and m and n are small, positive integers.
     """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # TODO: 2. Implement and test this function.
     #     The testing code is already written for you (above).
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
 
 def run_test_draw_wall_on_right():
     """ Tests the    draw_wall_on_right    function. """
     # Tests 1 and 2 are ALREADY DONE (here).
-    window = rg.RoseWindow(550, 300, 'Wall on the right, Tests 1 and 2')
+    window = rg.RoseWindow(550, 300, "Wall on the right, Tests 1 and 2")
 
-    window.continue_on_mouse_click('Click to run Test 1.')
+    window.continue_on_mouse_click("Click to run Test 1.")
 
     rectangle1 = rg.Rectangle(rg.Point(250, 30), rg.Point(250 + 30, 30 + 20))
     draw_wall_on_right(rectangle1, 8, window)
 
-    window.continue_on_mouse_click('Click to run Test 2.')
+    window.continue_on_mouse_click("Click to run Test 2.")
     rectangle2 = rg.Rectangle(rg.Point(470, 40), rg.Point(470 + 50, 40 + 50))
     draw_wall_on_right(rectangle2, 4, window)
 
@@ -104,7 +110,7 @@ def run_test_draw_wall_on_right():
 
 def draw_wall_on_right(rectangle, n, window):
     """
-    See   Walls.pdf   in this project for pictures that may
+    See   pictures_for_Walls.pdf   in this project for pictures that may
     help you better understand the following specification:
 
     Draws an n x n RIGHT-justified triangle of rectangles
@@ -120,13 +126,13 @@ def draw_wall_on_right(rectangle, n, window):
       :type window: rg.RoseWindow
     and n is a small, positive integer.
     """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #     The testing code is already written for you (above).
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
 
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 main()
